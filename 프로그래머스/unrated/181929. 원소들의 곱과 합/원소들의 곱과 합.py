2ce5ec7fd7a742f11fd54653c7bc73bada4b6a52
2ㅣ2ㅣ <106798875+cseeeeee@users.noreply.github.com>
2024-01-06 16:33:47 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,5 +1,4 @@
 from functools import reduce
 def solution(num_list):
     mul=reduce(lambda x,_: x*_, num_list)
-    sum=reduce(lambda y,_: y+_, num_list)
-    return 1 if mul<sum**2 else 0
+    return 1 if mul<sum(num_list)**2 else 0
