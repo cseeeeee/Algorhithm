ef74018e94687c337f2b77848185b20de0aae147
2ㅣ2ㅣ <106798875+cseeeeee@users.noreply.github.com>
2024-01-13 20:47:38 +0900

    [level Lv.0] Title: 수 조작하기 2, Time: 11.89 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/Lv.0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,12 +1,6 @@
 def solution(numLog):
     res=''
-    for i in range(len(numLog)-1):
-        if numLog[i+1] - numLog[i] == 1:
-            res+='w'
-        elif numLog[i+1] - numLog[i] == -1:
-            res+='s'
-        elif numLog[i+1] - numLog[i] == 10:
-            res+='d'
-        elif numLog[i+1] - numLog[i] == -10:
-            res+='a'
+    n_dict=dict(zip([1,-1,10,-10],['w','s','d','a']))
+    for i in range(0, len(numLog)-1):
+            res+=n_dict[numLog[i+1]-numLog[i]]
     return res
