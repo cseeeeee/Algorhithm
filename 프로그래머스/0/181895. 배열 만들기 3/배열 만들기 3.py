8da2bfbd9fba9543162da521bb1a321423ba2739
2ㅣ2ㅣ <106798875+cseeeeee@users.noreply.github.com>
2024-02-26 20:10:11 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,8 +1,13 @@
 def solution(arr, intervals):
     res=[]
-    for (i,v) in enumerate(intervals):
-        if i==0:
-            res.append(arr[v[0]:v[1]+1])
-        else:
-            res.append(arr[v[0]:v[1]+1])
-    return res[0]+res[1]
+    # for (i,v) in enumerate(intervals):
+    #     if i==0:
+    #         res.append(arr[v[0]:v[1]+1])
+    #     else:
+    #         res.append(arr[v[0]:v[1]+1])
+    # return res[0]+res[1]
+    
+    for a,b in intervals: res+=arr[a:b+1]
+    return res
+
+
