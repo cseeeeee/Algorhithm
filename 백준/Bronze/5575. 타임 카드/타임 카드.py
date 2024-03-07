19e83cec78acda9f8e5006d493285191c08b61d2
2ㅣ2ㅣ <106798875+cseeeeee@users.noreply.github.com>
2024-03-07 18:41:35 +0900

    [Bronze IV] Title: 타임 카드, Time: 64 ms, Memory: 38064 KB -BaekjoonHub

--- 백준/Bronze/5575. 타임 카드/타임 카드.py
+++ 백준/Bronze/5575. 타임 카드/타임 카드.py
@@ -1,21 +1,15 @@
 import datetime
 
-a=list(map(int, input().split())) #9 0 0 18 0 0
-b=list(map(int, input().split())) #9 0 1 18 0 0
-c=list(map(int, input().split())) #12 14 52 12 15 30
-employee=[a,b,c]
+a = list(map(int, input().split()))  # Example: 9 0 0 18 0 0
+b = list(map(int, input().split()))  # Example: 9 0 1 18 0 0
+c = list(map(int, input().split()))  # Example: 12 14 52 12 15 30
+employee = [a, b, c]
 
 for i in employee:
-  time1 = datetime.timedelta(hours=i[0], minutes=i[1], seconds=i[2])
-  time2 = datetime.timedelta(hours=i[3], minutes=i[4], seconds=i[5])
-  worktime=time2-time1
-
-  total_seconds = int(worktime.total_seconds())
-  # print(total_seconds)
-  hours = total_seconds // 3600
-  minutes = (total_seconds % 3600) // 60
-  seconds = total_seconds % 60
-  
-  # 콜론 대신 공백을 사용하여 시간 포맷을 만듭니다.
-  time_str = f"{hours} {minutes} {seconds}"
-  print(time_str)
+    time1 = datetime.timedelta(hours=i[0], minutes=i[1], seconds=i[2])  # 출근시간
+    time2 = datetime.timedelta(hours=i[3], minutes=i[4], seconds=i[5])  # 퇴근시간
+    worktime = time2 - time1  # 근무시간
+    
+    # Formatting each part to remove leading zeros
+    hours, minutes, seconds = str(worktime).split(':')
+    print(int(hours), int(minutes), int(seconds))
